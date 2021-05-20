ce07133edf34de8d73fbe9ada8faea86b045eaf2
Breno <breno.ga.duque@gmail.com>
2021-05-19 23:02:29 -0300

    Criando conexao com banco e fazendo as primeiras Migrations

---
+++ SiteLanches/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SiteLanches.Models;
+
+namespace SiteLanches.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Lanche> Lanches { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- SiteLanches/Models/Categoria.cs
+++ SiteLanches/Models/Categoria.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SiteLanches.Models
 {
     public class Categoria
     {
         public int CategoriaId { get; set; }
+        [StringLength(100)]
         public string Nome { get; set; }
+        [StringLength(200)]
         public string Descricao { get; set; }
         public List<Lanche> Lanches { get; set; }
     }
