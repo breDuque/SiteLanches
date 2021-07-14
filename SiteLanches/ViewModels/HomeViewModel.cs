0cdb5c7cee4be159de1de70622d0d85322f77598
Breno <breno.ga.duque@gmail.com>
2021-07-14 02:33:06 -0300

    Arquivos que ficaram para traz

---
+++ SiteLanches/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using SiteLanches.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiteLanches.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Lanche> LanchesPreferidos { get; set; }
+    }
+}
