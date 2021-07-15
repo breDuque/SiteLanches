a18450e05d2d4a7c2ca180c2a4f2ac5936961698
Breno <breno.ga.duque@gmail.com>
2021-07-15 01:11:40 -0300

    Adicionado component view do carrinho de compras

---
+++ SiteLanches/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SiteLanches.Models;
+using SiteLanches.ViewModels;
+
+namespace SiteLanches.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            //var itens = _carrinhoCompra.GetCarrinhoCompraItems();
+            var itens = new List<CarrinhoCompraItem>() {new CarrinhoCompraItem(), new CarrinhoCompraItem()};
+            _carrinhoCompra.CarrinhoCompraItens = itens;
+            var carrinhoCompraVm = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+            
+            return View(carrinhoCompraVm);
+        }
+    }
+}
