521ae9022481721e364dfe8a8fa547930604c861
Breno <breno.ga.duque@gmail.com>
2021-07-20 23:37:14 -0300

    Criado o listador de categoria

---
+++ SiteLanches/Components/CategoriaMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SiteLanches.Repositories.RepositoriesInterfaces;
+
+namespace SiteLanches.Components
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaMenu(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+        
+        public IViewComponentResult Invoke()
+        {
+            var categorias = _categoriaRepository.Categorias.OrderBy(c => c.Nome);
+            return View(categorias);
+        }
+    }
+}
